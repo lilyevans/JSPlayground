30661ec01e6c92c8ab922db2fd45dd049926cfb1
auroratide <tf.auroratide@gmail.com>
2016-11-15 14:30:32 -0600

    Extracted utils

--- js/lessons/multiplier/multiplierActions.js
+++ js/lessons/multiplier/multiplierActions.js
@@ -1,10 +1,11 @@
 import DOM from './multiplierDom';
+import Utils from './multiplierUtils';
 
 class MultiplierActions {
   static generateAnswer() {
     const lhs = DOM.getNumber('lhs');
     const rhs = DOM.getNumber('rhs');
-    DOM.setAnswer(lhs * rhs);
+    DOM.setAnswer(Utils.multiply(lhs, rhs));
   }
 }
 
---
+++ js/lessons/multiplier/multiplierUtils.js
@@ -0,0 +1,7 @@
+class MultiplierUtils {
+  static multiply(lhs, rhs) {
+    return lhs * rhs;
+  }
+}
+
+module.exports = MultiplierUtils;
