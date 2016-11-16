0232ab655115fafb89f388230fe4689515df4b58
sebradloff <sradloff23@gmail.com>
2016-11-15 21:14:22 -0500

    added top bar to allow easy navigation between exercises.

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const exercises = ['multiplier'];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Timothy's JS Fun Dive!!!", exercises });
 });
 
 router.get('/multiplier', function(req, res) {
-  res.render('multiplier', { title: 'Multiplier'});
+  res.render('multiplier', { title: 'Multiplier', exercises });
 });
 
 router.get('/emailValidator', function(req, res) {
